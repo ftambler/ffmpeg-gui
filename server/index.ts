e9b15ca9241f5d13f18f955a3789e5977eebfdf6
ftambler <federicotambler@gmail.com>
2026-02-21 20:52:36 -0300

    Listen on all interfaces

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,6 @@ app.use(express.json());
 
 app.use("/api", renderRoutes);
 
-app.listen(3001, () => {
-  console.log("Server running on http://localhost:3001");
+app.listen(3001, "0.0.0.0", () => {
+  console.log("Server running on port 3001");
 });
