50adc50642dd24f416cfe21b196531012336f540
ftambler <federicotambler@gmail.com>
2026-02-23 18:46:39 -0300

    Only mins and secs

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -11,14 +11,8 @@ export function timeToSeconds(time: string): number {
 
 export function formatTime(seconds: number): string {
   const safeSeconds = Number.isFinite(seconds) ? Math.max(0, seconds) : 0;
-  const hours = Math.floor(safeSeconds / 3600);
-  const mins = Math.floor((safeSeconds % 3600) / 60);
+  const mins = Math.floor((safeSeconds) / 60);
   const secs = Math.floor(safeSeconds % 60);
-//   const ms = Math.floor((safeSeconds % 1) * 100);
 
-  return `${hours}:${mins.toString().padStart(2, "0")}:${secs
-    .toString()
-    // .padStart(2, "0")}.}${ms
-    .toString()
-    .padStart(2, "0")}`;
+  return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
 }
