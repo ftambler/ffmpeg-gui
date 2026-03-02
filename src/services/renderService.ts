618efe617ba92dad22b923e928c0b4ee1786382e
ftambler <federicotambler@gmail.com>
2026-03-01 21:04:48 -0300

    Changed to receive jobId

--- src/services/renderService.ts
+++ src/services/renderService.ts
@@ -2,7 +2,7 @@ import { api } from "../api/axios";
 import type { MediaPayload } from "../types/MediaDraft";
 
 export class RenderService {
-  static async requestTimelineRender( media: MediaPayload[], outputFile: string): Promise<{ outputFile: string }> {
+  static async requestTimelineRender( media: MediaPayload[], outputFile: string): Promise<{ jobId: string }> {
     const response = await api.post("/video/render", { media, outputFile });
 
     return response.data;
